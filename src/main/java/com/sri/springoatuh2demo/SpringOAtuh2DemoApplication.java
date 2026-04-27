b69ab6148c9c462c82983e0f6c37d043acd2c37f
Srikanth-Devandla <srikanthdevendla@gmail.com>
2026-04-27 12:30:13 +0530

    first commit

---
+++ src/main/java/com/sri/springoatuh2demo/SpringOAtuh2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.sri.springoatuh2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOAtuh2DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOAtuh2DemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sri/springoatuh2demo/controller/SampleController.java
@@ -0,0 +1,15 @@
+package com.sri.springoatuh2demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping("/api") // if you want to uncomment this please add this url(http://localhost:9090/api/login/oauth2/code/github) in github or google
+public class SampleController {
+
+    @GetMapping("/")
+    public String greetMessage(){
+        return "Hello, Welcome to SpringSecurity OAuth2 Course..!";
+    }
+}
